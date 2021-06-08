54383ba8c714a5aeb902ecdaaed17c57d246e1cc
Grace Wang <gracewang92@gmail.com>
2021-06-08 12:17:14 -0700

    style using react bootstrap, include primary listing photo (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Results from './Results';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Results />
+        <h2>Live &lt; 15 min from Trader Joe's</h2>
+        <div>We love Trader Joe's and believe that it should be easier to find your dream home based off where the closest Trader Joe's is. Search below. :)</div>
       </header>
+      <Results />
     </div>
   );
 }
--- src/Results.tsx
+++ src/Results.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import './App.css';
-
-
-// https://stackoverflow.com/a/2901298
-function numberWithCommas(x: number) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
+import Listing from './Listing';
+import './Results.css';
 
 function Results() {
   const [distance, setDistance] = useState<number>(1);
   const [price, setPrice] = useState<number>(250_000);
+
   const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
 
   function selectDistance(e: any) {
     setDistance(e.target.value);
@@ -21,6 +21,7 @@ function Results() {
   }
 
   async function getResults() {
+    setLoading(true);
     const url = process.env.REACT_APP_TJ_HOUSE_BE_URL || 'http://localhost:5000';
     const res = await fetch(url, {
       method: 'post',
@@ -28,35 +29,49 @@ function Results() {
       body: JSON.stringify({ price, distance }),
     })
     setData(await res.json());
+    setLoading(false);
   }
 
   return (
     <div className="Results">
-      <div>Distance 📍</div>
-      <select value={distance} onChange={selectDistance}>
-        <option value={1}>&lt; 1 mi</option>
-        <option value={2}>&lt; 2 mi</option>
-        <option value={5}>&lt; 5 mi</option>
-        <option value={10}>&lt; 10 mi</option>
-        <option value={20}>&lt; 20 mi</option>
-      </select>
+      <Form className="results-form">
+        <Form.Group>
+          <Form.Label>Distance 📍</Form.Label>
+          <Form.Control as="select" value={distance} onChange={selectDistance}>
+            <option value={1}>&lt; 1 mi</option>
+            <option value={2}>&lt; 2 mi</option>
+            <option value={5}>&lt; 5 mi</option>
+            <option value={10}>&lt; 10 mi</option>
+            <option value={20}>&lt; 20 mi</option>
+          </Form.Control>
+        </Form.Group>
 
-      <div>Price 💸</div>
-      <select value={price} onChange={selectPrice}>
-        <option value={100_000}>&lt; $100K</option>
-        <option value={250_000}>&lt; $250K</option>
-        <option value={500_000}>&lt; $500K</option>
-        <option value={1_000_000}>&lt; $1M</option>
-        <option value={5_000_000}>&lt; $5M</option>
-      </select>
-      <button onClick={getResults}>Go!</button>
+        <Form.Group>
+          <Form.Label>Price 💸</Form.Label>
+          <Form.Control as="select" value={price} onChange={selectPrice}>
+            <option value={100_000}>&lt; $100K</option>
+            <option value={250_000}>&lt; $250K</option>
+            <option value={500_000}>&lt; $500K</option>
+            <option value={1_000_000}>&lt; $1M</option>
+            <option value={5_000_000}>&lt; $5M</option>
+          </Form.Control>
+        </Form.Group>
 
-      {data.map(d => <div key={d.id}>
-        <h3><a href={"https://redfin.com" + d.metadata.url}>{d.metadata.streetLine.value}</a></h3>
-        <div>${numberWithCommas(d.metadata.price.value)}</div>
-        <div>{d.closest_tj_distance_mi.toFixed(3)} mi away from {d.tj_name}</div>
-        <div>{d.tj_address}</div>
-      </div>)}
+        <Button className="form-submit" variant="primary" onClick={getResults}>
+          Go!
+          {loading && <Spinner className="loading-status" as="span" animation="grow" size="sm" role="status" aria-hidden="true" />}
+        </Button>
+      </Form>
+
+      <div className="listings">
+        {!loading && data.map(d => <Listing
+          key={d.id}
+          metadata={d.metadata}
+          closest_tj_distance_mi={d.closest_tj_distance_mi}
+          tj_name={d.tj_name}
+          tj_address={d.tj_address}
+        />)}
+      </div>
     </div>
   );
 }
