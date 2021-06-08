af7dae3e63e9085b268091c9d93910da3b25cb49
Grace Wang <gracewang92@gmail.com>
2021-06-08 15:16:45 -0400

    cleanup

--- src/Listing.tsx
+++ src/Listing.tsx
@@ -16,7 +16,7 @@ interface ListingProps {
 
 function Listing(props: ListingProps) {
   return (
-    <a className="listing" href={"https://redfin.com" + props.metadata.url} target="_blank">
+    <a className="listing" href={"https://redfin.com" + props.metadata.url} target="_blank" rel="noreferrer">
       <Card>
         <div className="card-img-wrapper"><Card.Img src={"https://ssl.cdn-redfin.com/photo/" + props.metadata.dataSourceId + "/bigphoto/" + props.metadata.mlsId.value.substr(-3) + "/" + props.metadata.mlsId.value + "_0.jpg"} /></div>
         <Card.Body>
