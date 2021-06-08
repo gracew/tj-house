75ef5cc616f9a5988fe83dc3fcb519d2712c8bb0
Grace Wang <gracewang92@gmail.com>
2021-06-08 13:43:51 -0700

    exclude land, parking spots, etc (#11)
    * exclude land, parking spots, etc
    
    * try to fix the favicon...

--- scripts/listings.sql
+++ scripts/listings.sql
@@ -4,6 +4,7 @@ CREATE TABLE listings (
     zip_code text,
     street_address text,
     price integer,
+    property_type integer,
     metadata jsonb,
     fetched_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     closest_tj_store_no integer,
@@ -13,3 +14,4 @@ ALTER TABLE ONLY listings ADD CONSTRAINT pk_listings_id PRIMARY KEY (id);
 CREATE INDEX idx_listings_zip_code ON listings USING btree (zip_code);
 CREATE INDEX idx_listings_closest_tj_distance_mi ON listings USING btree (closest_tj_distance_mi);
 CREATE INDEX idx_listings_closest_tj_distance_mi_price ON listings (closest_tj_distance_mi, price);
+CREATE INDEX idx_listings_closest_tj_distance_mi_price_property_type ON listings (closest_tj_distance_mi, price, property_type);
