15640a6a913cc2e8a2f16d2d7ff597d65bc30147
Andy Roberts <andy@note15.com>
2021-06-09 21:24:48 -0700

    state dropdown and $ ranges (#13)
    * state dropdown and $ ranges
    
    * format submit button
    
    * $ dropdown fix
    
    Co-authored-by: andy.roberts@easci.com <andy.roberts@easci.com>

--- scripts/app.js
+++ scripts/app.js
@@ -8,16 +8,24 @@ const client = new Client();
 app.use(express.json());
 app.use(cors())
 app.post('/', async (req, res) => {
+  const state = req.body.state;
   const distance = req.body.distance;
   const price = req.body.price;
   const offset = req.body.offset || 0;
   const pageSize = 60;
+  let priceLow;
+  if(price > 1000000) priceLow = 1000000;
+  else priceLow = price - 100000;
 
   const countQuery = `select count(*) from listings 
   where closest_tj_distance_mi < $1 
-  and price < $2
+  and price > $2
+  and price < $3
+  AND zip_code IN (
+    SELECT DISTINCT trader_joes.zip_code::VARCHAR FROM trader_joes WHERE state = $4
+  )
   and property_type in (1,2,3,5,6,7)`
-  const countRes = await client.query(countQuery, [distance, price]);
+  const countRes = await client.query(countQuery, [distance, priceLow, price, state]);
   const count = countRes.rows[0].count;
 
   const query = `select 
@@ -26,12 +34,16 @@ app.post('/', async (req, res) => {
   from listings l 
   left join trader_joes t on store_no = l.closest_tj_store_no
   where closest_tj_distance_mi < $1
-  and price < $2 
+  and price > $2
+  and price < $3 
+  and l.zip_code in (
+    SELECT DISTINCT trader_joes.zip_code::VARCHAR FROM trader_joes WHERE state = $4
+  )
   and property_type in (1,2,3,5,6,7)
   order by closest_tj_distance_mi 
-  offset $3
-  limit $4`;
-  const pgRes = await client.query(query, [distance, price, offset, pageSize]);
+  offset $5
+  limit $6`;
+  const pgRes = await client.query(query, [distance, priceLow, price, state, offset, pageSize]);
   res.json({
     count,
     rows: pgRes.rows,
