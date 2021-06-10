fc1ad0f357a0775c73b67ab226c59385d0f6510c
andy.roberts@easci.com <andy.roberts@easci.com>
2021-06-09 21:09:56 -0700

    state dropdown and $ ranges

--- src/Results.tsx
+++ src/Results.tsx
@@ -7,6 +7,7 @@ import './Results.css';
 
 function Results() {
   const url = process.env.REACT_APP_TJ_HOUSE_BE_URL || 'http://localhost:5000';
+  const [state, setState] = useState<any>('CA');
   const [distance, setDistance] = useState<number>(1);
   const [price, setPrice] = useState<number>(250_000);
 
@@ -15,6 +16,10 @@ function Results() {
   const [paging, setPaging] = useState<any>();
   const [loading, setLoading] = useState(false);
   const [moreLoading, setMoreLoading] = useState(false);
+
+  function selectState(e: any) {
+    setState(e.target.value);
+  }
 
   function selectDistance(e: any) {
     setDistance(e.target.value);
@@ -29,7 +34,7 @@ function Results() {
     const res = await fetch(url, {
       method: 'post',
       headers: { "content-type": "application/json" },
-      body: JSON.stringify({ price, distance }),
+      body: JSON.stringify({ price, distance, state }),
     })
     const parsed = await res.json();
     setCount(parsed.count);
@@ -43,7 +48,7 @@ function Results() {
     const res = await fetch(url, {
       method: 'post',
       headers: { "content-type": "application/json" },
-      body: JSON.stringify({ price, distance, offset: paging.nextOffset }),
+      body: JSON.stringify({ price, distance, state, offset: paging.nextOffset }),
     })
     const parsed = await res.json();
     setRows([...rows, ...parsed.rows])
@@ -55,6 +60,60 @@ function Results() {
     <div className="Results">
       <Form className="results-form">
         <Form.Group>
+        <Form.Label>State 🇺🇸</Form.Label>
+          <Form.Control as="select" value={state} onChange={selectState}>
+            <option value={'AL'}>Alabama</option>
+            <option value={'AK'}>Alaska</option>
+            <option value={'AZ'}>Arizona</option>
+            <option value={'AR'}>Arkansas</option>
+            <option value={'CA'}>California</option>
+            <option value={'CO'}>Colorado</option>
+            <option value={'CT'}>Connecticut</option>
+            <option value={'DE'}>Delaware</option>
+            <option value={'DC'}>District Of Columbia</option>
+            <option value={'FL'}>Florida</option>
+            <option value={'GA'}>Georgia</option>
+            <option value={'HI'}>Hawaii</option>
+            <option value={'ID'}>Idaho</option>
+            <option value={'IL'}>Illinois</option>
+            <option value={'IN'}>Indiana</option>
+            <option value={'IA'}>Iowa</option>
+            <option value={'KS'}>Kansas</option>
+            <option value={'KY'}>Kentucky</option>
+            <option value={'LA'}>Louisiana</option>
+            <option value={'ME'}>Maine</option>
+            <option value={'MD'}>Maryland</option>
+            <option value={'MA'}>Massachusetts</option>
+            <option value={'MI'}>Michigan</option>
+            <option value={'MN'}>Minnesota</option>
+            <option value={'MS'}>Mississippi</option>
+            <option value={'MO'}>Missouri</option>
+            <option value={'MT'}>Montana</option>
+            <option value={'NE'}>Nebraska</option>
+            <option value={'NV'}>Nevada</option>
+            <option value={'NH'}>New Hampshire</option>
+            <option value={'NJ'}>New Jersey</option>
+            <option value={'NM'}>New Mexico</option>
+            <option value={'NY'}>New York</option>
+            <option value={'NC'}>North Carolina</option>
+            <option value={'ND'}>North Dakota</option>
+            <option value={'OH'}>Ohio</option>
+            <option value={'OK'}>Oklahoma</option>
+            <option value={'OR'}>Oregon</option>
+            <option value={'PA'}>Pennsylvania</option>
+            <option value={'RI'}>Rhode Island</option>
+            <option value={'SC'}>South Carolina</option>
+            <option value={'SD'}>South Dakota</option>
+            <option value={'TN'}>Tennessee</option>
+            <option value={'TX'}>Texas</option>
+            <option value={'UT'}>Utah</option>
+            <option value={'VT'}>Vermont</option>
+            <option value={'VA'}>Virginia</option>
+            <option value={'WA'}>Washington</option>
+            <option value={'WV'}>West Virginia</option>
+            <option value={'WI'}>Wisconsin</option>
+            <option value={'WY'}>Wyoming</option>
+          </Form.Control>
           <Form.Label>Distance 📍</Form.Label>
           <Form.Control as="select" value={distance} onChange={selectDistance}>
             <option value={1}>&lt; 1 mi</option>
@@ -69,10 +128,16 @@ function Results() {
           <Form.Label>Price 💸</Form.Label>
           <Form.Control as="select" value={price} onChange={selectPrice}>
             <option value={100_000}>&lt; $100K</option>
-            <option value={250_000}>&lt; $250K</option>
-            <option value={500_000}>&lt; $500K</option>
-            <option value={1_000_000}>&lt; $1M</option>
-            <option value={5_000_000}>&lt; $5M</option>
+            <option value={200_000}>$100K - $200K</option>
+            <option value={300_000}>$200K - $300K</option>
+            <option value={400_000}>$300K - $400K</option>
+            <option value={500_000}>$400K - $500K</option>
+            <option value={500_000}>$500K - $600K</option>
+            <option value={500_000}>$600K - $700K</option>
+            <option value={500_000}>$700K - $800K</option>
+            <option value={500_000}>$800K - $900K</option>
+            <option value={1_000_000}>$900K - $1M</option>
+            <option value={500_000_000}>&gt; $1M</option>
           </Form.Control>
         </Form.Group>
 
